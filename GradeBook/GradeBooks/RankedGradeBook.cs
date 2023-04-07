9dd13f390f16e684c0810f686ee23125bb1f2e6c
MJagocki <jagockim@gmail.com>
2023-04-07 22:50:16 +0200

    Override `RankedGradeBook`'s `GetLetterGrade` method (done)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,10 +23,20 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException();
             }
-            
-            var seperatedCountStudents =
-            
-            return base.GetLetterGrade(averageGrade);
+
+            var seperatedCountStudents = (int)Math.Ceiling(Students.Count * 0.2);
+            var studentGrades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= studentGrades[seperatedCountStudents - 1])
+                return 'A';
+            if (averageGrade >= studentGrades[(seperatedCountStudents * 2) -1 ])
+                return 'B';
+            if (averageGrade >= studentGrades[(seperatedCountStudents * 3) - 1])
+                return 'C';
+            if (averageGrade >= studentGrades[(seperatedCountStudents * 4) - 1])
+                return 'D';
+
+            return 'F';
         }
 
     }
