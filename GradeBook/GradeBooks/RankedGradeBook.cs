99af147248bfe07321c2e0f4ee6be587bf5892ed
MJagocki <jagockim@gmail.com>
2023-04-07 22:40:29 +0200

    Creating the `StandardGradeBook` class
    Creating the `RankedGradeBook` class
    
    Override `RankedGradeBook`'s `GetLetterGrade` method(started)

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,35 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    
+     public class RankedGradeBook : BaseGradeBook
+    {
+
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            var seperatedCountStudents =
+            
+            return base.GetLetterGrade(averageGrade);
+        }
+
+    }
+
+}
+
